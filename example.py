c1cc559e59cfe931b7be1a71910e16f1ed2cba7c
bernard <bbn.bernard@yahoo.com>
2015-08-12 13:54:10 +0700

    add more test (requirement) and update main class

--- example.py
+++ example.py
@@ -1,19 +1,24 @@
 class SimpleNumber:
 
     def __init__(self, num):
+        val = self.is_number(num)
+        
+        self.x = val
+        
+    def is_number(self, x):
         try:
-            val = float(num)
+            val = float(x)
         except ValueError:
             print("That's not a float number!")
+
+        return val
         
-        self.x = num
-  
     def add(self, y):
-        result = self.x * y
+        result = self.x + self.is_number(y)
 
         return result
 
     def multiply(self, y):
-        result = self.x * y
+        result = self.x * self.is_number(y)
 
         return result
--- test_example.py
+++ test_example.py
@@ -6,6 +6,16 @@ class TestSimpleNumber(unittest.TestCase):
     def test_simple(self):
         self.assertEqual(4, ex.SimpleNumber(1).add(3))
         self.assertEqual(6, ex.SimpleNumber(2).multiply(3))
+        
+        self.assertAlmostEqual(0.06, ex.SimpleNumber(0.2).multiply(0.3))
+        
+    def test_from_str(self):
+        self.assertEqual(4, ex.SimpleNumber('1').add(3))
+        
+        self.assertEqual(4, ex.SimpleNumber('1').add('3'))
+        
+        self.assertAlmostEqual(0.06, ex.SimpleNumber('0.2').multiply('0.3'))
+
 
 if __name__ == '__main__':
     unittest.main()
